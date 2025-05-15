d5c6554e54545124dd359dc71556e661643a77ef
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-05-15 20:20:17 +0200

    task bit strings added

--- CSES/1.9bit_strings.cpp
+++ CSES/1.9bit_strings.cpp
@@ -2,10 +2,25 @@
 #define LL long long
 using namespace std;
 
+LL get_permutations(int n){
+    LL res = 1; 
+    while(n){
+        res <<= 1;
+        res %= 1000000007; 
+        n--; 
+    }
+
+    return res;
+}
+
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
 
-    LL y, x; 
-    int t; 
+    int n; 
+    cin >> n; 
+
+    cout << get_permutations(n);
+
+    return 0;
 }
